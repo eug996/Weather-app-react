3e52d31bb633acb1d1122b44228fb4f2f658b37d
eug996 <104517572+eug996@users.noreply.github.com>
2023-03-29 21:04:54 +0100

    add developed

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+  import WeatherForecast from "./WeatherForecast";
+  
+  export default function Home() {
+    const [searchTerm, setSearchTerm] = useState("");
+  return (
+      <>
+        <form role="search">
+          <input
+            type="search"
+            aria-label="Search for a city"
+            placeholder="London"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+          <button  type="button">
+            Search
+          </button>
+        </form>
+        <WeatherForecast />
+      </>
+    );
+  }
